62c8726a8f950ea51263cdf8c9a95d08f45f14d5
Scott Adler <enclave38@gmail.com>
2023-06-03 22:38:12 +0300

    Домашняя работа №21

---
+++ src/main/java/com/homework19/homework19/EmployeeController.java
@@ -0,0 +1,35 @@
+package com.homework19.homework19;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService service;
+
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+    @GetMapping(path = "/add")
+    public Employee addEmployee (@RequestParam String firstName, @RequestParam String lastName){
+        return service.add(firstName,lastName);
+    }
+    @GetMapping(path = "/remove")
+    public Employee removeEmployee (@RequestParam String firstName, @RequestParam String lastName){
+        return service.remove(firstName,lastName);
+    }
+    @GetMapping(path = "/find")
+    public Employee findEmployee (@RequestParam String firstName, @RequestParam String lastName){
+        return service.find(firstName,lastName);
+    }
+    @GetMapping
+    public Collection<Employee> findAll() {
+        return service.findAll();
+    }
+}
