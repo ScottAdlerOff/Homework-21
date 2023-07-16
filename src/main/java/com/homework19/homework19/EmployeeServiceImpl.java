4787c72f7ee789371b7ba997ce8a936167913a59
Scott Adler <enclave38@gmail.com>
2023-07-17 02:53:29 +0300

    Домашняя работа №26

--- src/main/java/com/homework19/homework19/EmployeeServiceImpl.java
+++ src/main/java/com/homework19/homework19/EmployeeServiceImpl.java
@@ -1,58 +1,61 @@
 package com.homework19.homework19;
 
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import java.util.*;
+
+import static org.springframework.util.StringUtils.capitalize;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     private final Map<String, Employee> employees;
+    private final EmployeeValidationService validationService;
 
-    public EmployeeServiceImpl() {
+    public EmployeeServiceImpl(EmployeeValidationService validationService) {
+
+        this.validationService = validationService;
         this.employees = new HashMap<>();
     }
 
     @Override
-    public Employee add(String firstName, String lastName, int salary, int department) {
-        validateInput(firstName,lastName);
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFullName())) {
-            throw new EmployeeAlreadyAddedException();
-        }
-        employees.put(employee.getFullName(), employee);
+    public Employee add(String firstName, String lastName, int salary, int departmentId) {
+        validationService.validate(firstName,lastName);
+        return addEmployee(new Employee(capitalize(firstName), capitalize(lastName), salary, departmentId));
+    }
+
+    @Override
+    public Employee add(String firstName, String lastName){
+        validationService.validate(firstName,lastName);
+        return addEmployee(new Employee(capitalize(firstName), capitalize(lastName)));
+    }
+
+    @Override
+    public Employee remove(String firstName, String lastName) {
+        Employee employee = find(firstName, lastName);
+        employees.remove(employee.getFullName());
         return employee;
     }
 
     @Override
-    public Employee remove(String firstName, String lastName, int salary, int department) {
-        validateInput(firstName,lastName);
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFullName())) {
-            return employees.remove(employee.getFullName());
+    public Employee find(String firstName, String lastName) {
+        String fullNameKey = firstName + " " + lastName;
+        if (!employees.containsKey(fullNameKey)) {
+            throw new EmployeeNotFoundException();
         }
-        throw new EmployeeNotFoundException();
-    }
-
-    @Override
-    public Employee find(String firstName, String lastName, int salary, int department) {
-        validateInput(firstName,lastName);
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFullName())) {
-            return employees.get(employee.getFullName());
-        }
-        throw new EmployeeNotFoundException();
+        return employees.get(fullNameKey);
     }
 
 
     @Override
     public Collection<Employee> findAll() {
-        return Collections.unmodifiableCollection(employees.values());
+        return employees.values();
     }
 
-    private void validateInput(String firstName, String lastName){
-        if (!(isAlpha(firstName) && isAlpha(lastName))){
-            throw new InvalidInputException();
+    private Employee addEmployee(Employee employee){
+        if (employees.containsKey(employee.getFullName())){
+            throw new EmployeeAlreadyAddedException();
         }
+        employees.put(employee.getFullName(), employee);
+        return employee;
     }
 }
